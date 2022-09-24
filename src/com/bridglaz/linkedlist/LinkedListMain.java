4836cb310bcd7501e65fa4a3bbf0fd335a421937
sujit <sujitbokade2014@gmail.com>
2022-09-24 12:52:16 +0530

    [Sujit]Adding Elements

---
+++ src/com/bridglaz/linkedlist/LinkedListMain.java
@@ -0,0 +1,39 @@
+package com.bridglaz.linkedlist;
+
+public class LinkedListMain<T> {
+    Node<T> head;
+    Node<T> tail;
+
+    public void addElement(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void print() {
+        Node<T> temp = head;
+        if (temp == null) {
+            System.out.println("LinkedList is Empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.data + "->");
+                temp = temp.next;
+            }
+            System.out.println("null");
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListMain<Integer> list = new LinkedListMain<>();
+        list.addElement(70);
+        list.addElement(30);
+        list.addElement(56);
+        list.print();
+
+    }
+}
