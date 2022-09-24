0f214ffcc5f11807a64dddf4e207a681613bb26c
sujit <sujitbokade2014@gmail.com>
2022-09-24 12:58:17 +0530

    [Sujit]Append Elements

--- src/com/bridglaz/linkedlist/LinkedListMain.java
+++ src/com/bridglaz/linkedlist/LinkedListMain.java
@@ -15,6 +15,17 @@ public class LinkedListMain<T> {
         }
     }
 
+    public void appendElement(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
     public void print() {
         Node<T> temp = head;
         if (temp == null) {
@@ -30,10 +41,14 @@ public class LinkedListMain<T> {
 
     public static void main(String[] args) {
         LinkedListMain<Integer> list = new LinkedListMain<>();
-        list.addElement(70);
-        list.addElement(30);
-        list.addElement(56);
-        list.print();
+//        list.addElement(70);
+//        list.addElement(30);
+//        list.addElement(56);
+//        list.print();
 
+        list.appendElement(56);
+        list.appendElement(30);
+        list.appendElement(70);
+        list.print();
     }
 }
